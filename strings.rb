9ffd88b0e639ea808f3aa774cee21f306c8be563
Subramaniam G <sgopalakrishnan0608@hl.com>
2019-01-03 14:15:36 -0800

    learn strings

---
+++ strings.rb
@@ -0,0 +1,20 @@
+s1 = "Hello\nWorld"
+s2 = 'Hello\nWorld'
+s3 = ".NET"
+s4 = "Ruby on Rails for #{s3} Developers"
+s5 = "Ruby on Rails for " + s3 + " Developers"
+
+puts s1
+puts s2
+puts s3
+puts s4
+puts s5
+
+puts "Hello".length
+puts "Hello"[1]
+puts "Hello".split("e")
+puts "Hello Ruby".gsub("Hello", "Goodbye")
+puts "Hello Ruby".replace("Goodbye")
+puts "HELLO RUBY".downcase
+puts "hello ruby".upcase
+puts "hello Ruby".capitalize
